73d3f3d73a24f24afe1452bbbd0960d624ac3084
Vladislav Hilko <uladzislau.hilko@gmail.com>
2024-03-17 16:10:20 +0100

    Update scrapper

--- lib/oddsportal_scraper/scrapers/matches.rb
+++ lib/oddsportal_scraper/scrapers/matches.rb
@@ -14,25 +14,30 @@ module OddsportalScraper
         @sport = sport
         @country = country.downcase
         @league = league.downcase.split(' ').join('-')
-        @season = season.split('/').join('-')
+        season_starts, season_ends = season.split(/[-\/]/)
+        @season = "#{season_starts}-#{season_ends}"
 
         @path = "#{@sport}/#{@country}/#{@league}-#{@season}/results/"
         url = "https://www.oddsportal.com/#{@path}"
 
-        parse!(:parse, url:)
+        parse!(:parse, url:, data: { season_starts: season_starts, season_ends: season_ends })
       end
 
-      def parse(response, url:, **)
-        return [] if page_does_not_exist?(response) || no_odds_available_for_the_season?(response)
+      def parse(response, url:, data:, **)
+        browser.visit("#{url}#/page/#{1}/")
+        parse_page(season_starts: data[:season_starts], season_ends: data[:season_ends])
 
-        last_page_num = response.css('div#pagination a').last.attributes.fetch('href').value.split('/').last.to_i
-        all_season_matches = []
 
-        (1..last_page_num).each do |page_number|
-          browser.visit("#{url}#/page/#{page_number}/")
-          all_season_matches.concat(parse_page_with_matches)
-        end
-        all_season_matches
+        # return [] if page_does_not_exist?(response) || no_odds_available_for_the_season?(response)
+
+        # last_page_num = response.css('div#pagination a').last.attributes.fetch('href').value.split('/').last.to_i
+        # all_season_matches = []
+
+        # (1..last_page_num).each do |page_number|
+        #   browser.visit("#{url}#/page/#{page_number}/")
+        #   all_season_matches.concat(parse_page_with_matches)
+        # end
+        # all_season_matches
       end
 
       private
@@ -49,30 +54,40 @@ module OddsportalScraper
         )
       end
 
-      def parse_page_with_matches # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-        tournament_table = browser.current_response.css('div#tournamentTable')
+      def parse_page(season_starts:, season_ends:)
+        scroll_till_the_end
 
+        elements_with_dash = browser.find_all(:xpath, "//a[contains(text(), '–')]")
         matches = []
-        match_date = nil
-        tournament_table.css('tr').each do |match_day|
-          if match_day.css('span.datet').text.present?
-            match_date = match_day.css('span.datet').text
-          elsif match_day.css('td.table-time').text.present?
-            match_time = match_day.css('td.table-time').text
-            participants = match_day.css('td.table-participant a').text
-            score = match_day.css('td.table-score').text
-            home_win, draw, away_win = match_day.css('td.odds-nowrp a').children.map(&:to_s)
+        elements_with_dash.each do |element|
+          parent_with_class = element.find(:xpath, './ancestor::div[contains(@class, "group") and contains(@class, "flex")]')
+          date = parent_with_class.find_all(:xpath, "./ancestor::*/preceding::*[contains(text(), #{season_starts}) or contains(text(), #{season_ends})][1]").map(&:text).last
+          match_row = parent_with_class.find_all('.//*[not(*) and normalize-space()]')
 
-            matches << {
-              match_date:,
-              match_time:,
-              participants:,
-              score:,
-              odds: { home_win:, draw:, away_win: }
-            }
-          end
+          matches << match_row_hash(match_row.map(&:text), date: date)
         end
         matches
+      end
+
+      def scroll_till_the_end
+        browser.execute_script("window.scrollBy(0,10000)")
+        sleep 1
+        browser.execute_script("window.scrollBy(0,10000)")
+        sleep 1
+      end
+
+      def match_row_hash(match_row, date:)
+        {
+          match_date: date,
+          match_time: match_row[0],
+          participants: "#{match_row[1]} - #{match_row[6]}",
+          score: "#{match_row[3]}:#{match_row[5]}",
+          odds: {
+            home_win: match_row[9],
+            draw: match_row[10],
+            away_win: match_row[11]
+          }
+        }
       end
 
     end
